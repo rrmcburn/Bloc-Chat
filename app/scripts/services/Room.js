772f3b23ec405afe83931886b5df732fcc26bced
Ryan McBurney <rrmcburn@gmail.com>
2017-03-01 16:31:24 -0500

    added list of rooms, still need to style

---
+++ app/scripts/services/Room.js
@@ -0,0 +1,18 @@
+(function() {
+  function Room($firebaseArray) {
+    var ref = firebase.database().ref().child("rooms");
+    var rooms = $firebaseArray(ref);
+
+    return {
+      all: rooms,
+//      create: function(roomName){
+//          rooms.$add({name: roomName});
+//      }        
+    };
+      
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Room', ['$firebaseArray', Room]);
+})();
