a649c60efce312fbd67dc6ee88958649e2948485
Ryan McBurney <rrmcburn@gmail.com>
2017-03-04 14:59:20 -0500

    connected messages to chat section

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-    function HomeCtrl($scope, $uibModal, Room) {
+    function HomeCtrl($scope, $uibModal, Room, Message) {
       $scope.rooms = Room.all;
       $scope.activeRoom = null;
       $scope.roomName = null;     
@@ -17,12 +17,13 @@
       
       $scope.activeRoomSet = function(room) {
           $scope.activeRoom = room;
-          $scope.roomName = room.name; 
+          $scope.roomName = room.name;
+          $scope.messages = Message.getByRoomId(room.$id);
       }
       
     }
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['$scope', '$uibModal', 'Room', HomeCtrl] );
+        .controller('HomeCtrl', ['$scope', '$uibModal', 'Room', 'Message', HomeCtrl] );
 })();
