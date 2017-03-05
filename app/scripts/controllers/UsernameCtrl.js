f19b816735b26beab4d144c770e8046ea69c6229
Ryan McBurney <rrmcburn@gmail.com>
2017-03-05 16:54:24 -0500

    added new files

---
+++ app/scripts/controllers/UsernameCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+    function UsernameCtrl($scope, $uibModalInstance){
+        
+    }
+
+    angular
+        .module('blocChat')
+        .controller('UsernameCtrl', ['$scope', '$uibModalInstance', UsernameCtrl])
+})();
